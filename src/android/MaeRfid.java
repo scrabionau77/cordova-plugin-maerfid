4725349268a406d62e6d79b82f49751389a41535
Daniele Pellerucci <dpellerucci@gmail.com>
2019-09-02 15:01:04 +0200

    First Caen method

--- src/android/MaeRfid.java
+++ src/android/MaeRfid.java
@@ -12,60 +12,66 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.util.Log;
+import com.caen.RFIDLibrary.*;
 
+import android.util.Log;
 import java.util.Date;
 
 public class MaeRfid extends CordovaPlugin {
-  private static final String TAG = "MaeRfid";
+    private static final String TAG = "MaeRfid";
 
-  /**
-     * Constructor.
-     */
-  public MaeRfid() {
-  } 
+    /**
+       * Constructor.
+       */
+    public MaeRfid() {
+    } 
 
-  
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
+    
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
 
-    Log.d(TAG, "Initializing MaeRfid");
-  }
-
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    if(action.equals("echo")) {
-      String phrase = args.getString(0);
-      // Echo back the first argument
-      Log.d(TAG, phrase);
-
-    } else if(action.equals("getDate")) {
-      // An example of returning data back to the web layer
-      final PluginResult result = new PluginResult(PluginResult.Status.OK, (new Date()).toString());
-      callbackContext.sendPluginResult(result);
-
-    } else if(action.equals("bestemmia")) {
-      JSONObject r = new JSONObject();
-      r.put("dio", "porco");
-      callbackContext.success(r);
-
-    } else if(action.equals("rfid")) {
-
-      // Execute in another thread to avoid blocking
-      cordova.getThreadPool().execute(new Runnable() {
-        public void run() {
-            // Qui va chiamata la libreria android CAEN RFID
-            if(true) {
-                // Execute success callback
-                callbackContext.success("qualcosa");
-            } else {
-                // Execute error callback
-                callbackContext.error("ko");
-            }
-        }
-      });
-
+        Log.d(TAG, "Initializing MaeRfid");
     }
-    return true;
-  }
+
+    public boolean execute(String action, JSONArray params, final CallbackContext callbackContext) throws JSONException {
+      
+        String args = params.getString(0);
+
+        if(action.equals("echo")) {
+            String phrase = args.getString(0);
+            // Echo back the first argument
+            Log.d(TAG, args);
+
+        } else if(action.equals("getDate")) {
+            // An example of returning data back to the web layer
+            final PluginResult result = new PluginResult(PluginResult.Status.OK, (new Date()).toString());
+            callbackContext.sendPluginResult(result);
+
+        } else if(action.equals("bestemmia")) {
+            JSONObject r = new JSONObject();
+            r.put("dio", "porco");
+            callbackContext.success(r);
+
+        } else if(action.equals("rfid")) {
+
+            // Execute in another thread to avoid blocking
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    // Qui va chiamata la libreria android CAEN RFID
+                    CAENRFIDReader reader = new CAENRFIDReader();
+                    try {
+                        // Execute success callback
+                        reader.Connect(CAENRFIDPort.CAENRFID_RS232, "");
+
+                        callbackContext.success("qualcosa");
+                    } catch (CAENRFIDException e) {
+                        callbackContext.error(e.toString()); // Execute error callback
+                    }
+                }
+            });
+
+        }
+        return true;
+    }
 
 }
