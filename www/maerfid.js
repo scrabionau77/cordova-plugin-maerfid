6be2a7bc4d7e0c5f63023d281dd16df81ae884ae
Daniele Pellerucci <dpellerucci@gmail.com>
2019-09-09 17:50:38 +0200

    Fix

--- www/maerfid.js
+++ www/maerfid.js
@@ -4,50 +4,6 @@
  * Copyright (c) 209, Daniele Pellerucci @ Maestrale Information Technology
  */
 
-
-var exec = cordova.require("cordova/exec");
-
-/**
- * Constructor.
- *
- * @returns {MaeRfid}
- */
-function MaeRfid() {
-}
-
-
-
-
-
-
-
-
-//-------------------------------------------------------------------
-MaeRfid.prototype.requestPermission = function (opts, successCallback, errorCallback) {
-    if (typeof opts === 'function') {  //user did not pass opts
-        errorCallback = successCallback;
-        successCallback = opts;
-        opts = {};
-    }
-    if (errorCallback == null) {
-        errorCallback = function () {
-        };
-    }
-
-    if (typeof errorCallback != "function") {
-        console.log("MaeRfid.requestPermission failure: error callback parameter not a function");
-        return;
-    }
-
-    if (typeof successCallback != "function") {
-        console.log("MaeRfid.requestPermission failure: success callback parameter must be a function");
-        return;
-    }
-
-    exec(successCallback, errorCallback, 'MaeRfid', 'requestPermission', [{'opts': opts}]);
-};
-
-
 var maerfid = {
     requestPermission: function (opts, successCallback, errorCallback) {
         if (typeof opts === 'function') {  //user did not pass opts
@@ -74,6 +30,5 @@ var maerfid = {
     }
 }
 
-//var maeRfid = new MaeRfid();
-//module.exports = maeRfid;
+
 module.exports = maerfid;
