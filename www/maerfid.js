d2436523bcb02e9731ecd2d646533a187ec8b0ac
Daniele Pellerucci <dpellerucci@gmail.com>
2019-09-09 17:52:13 +0200

    Fix

--- www/maerfid.js
+++ www/maerfid.js
@@ -4,6 +4,11 @@
  * Copyright (c) 209, Daniele Pellerucci @ Maestrale Information Technology
  */
 
+
+var exec = cordova.require("cordova/exec");
+
+
+
 var maerfid = {
     requestPermission: function (opts, successCallback, errorCallback) {
         if (typeof opts === 'function') {  //user did not pass opts
